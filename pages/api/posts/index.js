53b33bbf6b556f6b52cd684d00b0f8cd1c40bc92
Tonichris01 <chineduokatta1000@gmail.com>
2024-11-01 15:29:27 +0100

    create Add ost API and Page

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -13,8 +13,27 @@ export default async function handler(req, res) {
         }finally{
             await prisma.$disconnect();
         }
-    }else{
-        res.setHeader('Aallow', ['GET']);
+        // creating a new blog
+    }else if(req.method == 'POST'){
+        const {title, content} = req.body;
+
+        try {
+            const newPost = await prisma.post.create({
+                data: {
+                    title,
+                    content
+                }
+            });
+
+            res.status(201).json(newPost);
+        } catch(error){
+            console.error("Error creating the post:", error);
+            res.status(500).json({message: "Error creating the post"});
+        }
+    }
+    else{
+        res.setHeader('Aallow', ['GET', 'POST']);
         res.status(405).end('Method not allowed');
     }
 }
+
